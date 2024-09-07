239074dbb4e000d652d68f819d87545156398d2b
Aditi Tapariya <tapariyaaditi23@gmail.com>
2024-09-07 15:29:14 +0530

    day 10 updated

---
+++ DAY 10/dff.v
@@ -0,0 +1,26 @@
+module dff(
+    input D,
+    input clk,
+    input sync_reset,
+    output reg Q,
+    output reg Qbar
+);
+
+//synchronous flip flops
+
+wire Qbarwire;
+
+not not1(Qbarwire, Qbar);
+
+always @(posedge clk) 
+    begin
+    if (sync_reset) 
+        Q <= 1'b0;
+     else
+        Q <= D;
+    end
+
+always @(*)
+    Qbar = Qbarwire;
+
+endmodule
